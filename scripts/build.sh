3d9bf2ec051dfc8151b51cc3c64b8fc4a081d7f9
spalger <email@spalger.com>
2016-06-24 17:58:34 -0700

    update build script to match new plugin specification

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,32 +2,31 @@
 
 set -e;
 
-version="${1:-$npm_package_version}"
-
-if [[ ! -n "$version" ]]; then
-  echo "you need to use 'npm run build', or pass the version as the first arg to the build script."
-  env
+if [ ! -f package.json ]; then
+  echo "this script must be run from the root of the kibana-site-plugins repo"
   exit 1
 fi
 
-workspace="build"
-proj="site_plugins"
-dir="$workspace/$proj"
-archive="$proj-$version.zip"
+set -x;
+
+root="$(pwd)"
+proj="$(jq -r .name package.json)"
+version="$(jq -r .version package.json)"
+build="$root/build"
+plugin="$build/kibana/$proj"
+archiveName="$proj-$version.zip"
 
 # out with the old
-rm -rf "$workspace";
+rm -rf "$build";
 
 # in with the new
-mkdir -p "$dir";
-cp -r public index.js site_plugins package.json "$dir";
+mkdir -p "$plugin";
+cp -r public index.js site_plugins package.json "$plugin";
 
 # install prod deps
-cd "$dir"
+cd "$plugin"
 npm install --production
-cd -
 
-# make an archive
-cd "$dir/.."
-zip -r "$archive" "$proj"
-cd -
+# zip it up
+cd "$build"
+zip -r "$archiveName" "kibana"
